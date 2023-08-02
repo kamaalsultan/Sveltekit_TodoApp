05e12de5ef465da1f9f586be14d580508f85ae62
ByteBallet <shinystar0314@gmail.com>
2023-08-01 22:24:48 -0400

    Load Env Variables

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,14 +3,14 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-	apiKey: "AIzaSyBC8U3UNUUYXBh-oM4dIH56YbiPpD8ksSc",
-    authDomain: "todoapp-0728.firebaseapp.com",
-    databaseURL: "https://todoapp-0728-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "todoapp-0728",
-    storageBucket: "todoapp-0728.appspot.com",
-    messagingSenderId: "237229524940",
-    appId: "1:237229524940:web:c177c746f4a93829c9ff69",
-    measurementId: "G-47275DNC32"
+	apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    databaseURL: import.meta.env.VITE_DATABASE_URL,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_APP_ID,
+    measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
 
 let firebaseApp!: FirebaseApp;
